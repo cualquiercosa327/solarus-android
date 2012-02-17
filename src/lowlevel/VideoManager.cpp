161927bf02403df666c23a802d0315864d033351
Samuel Lepetit <gsam101@gmail.com>
2012-02-17 17:44:39 +0100

    Android specific fix (aimed at lowend devices, where the other modes are slow)

--- src/lowlevel/VideoManager.cpp
+++ src/lowlevel/VideoManager.cpp
@@ -190,7 +190,11 @@ void VideoManager::set_initial_video_mode() {
  * @brief Sets the default video mode.
  */
 void VideoManager::set_default_video_mode() {
+#ifdef ANDROID
+  set_video_mode(WINDOWED_NORMAL);
+#else
   set_video_mode(WINDOWED_STRETCHED);
+#endif
 }
 
 /**
